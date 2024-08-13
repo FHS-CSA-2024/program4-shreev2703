e6c7a767da4379bf39b7c9ab1e843d0ca832150d
shreev2703 <67807046+shreev2703@users.noreply.github.com>
2024-08-13 10:11:05 -0700

    ShreevatsonBabuSreegeethaProgram4.java

--- Program4.java
+++ Program4.java
@@ -1,6 +1,31 @@
-//Youre on your own!
-//Jk ask for help if you need it (but also look at previous programs!!)
+//Program 3
+//Extension of program 2 that now asks for user input when assigning the length and width of the sides
+import java.util.*;  //This imports the console scanner that reads user input
 
+//Your code here:
+public class ScannerRectangle{
+    public static void main(String args[])
+    {
+    Scanner scanner= new Scanner(System.in);
+    System.out.println("Enter your desired number1:");
+    int number1 = scanner.nextInt(); // Read length
+    
+    System.out.println("Enter your desired number2:");
+    int number2 = scanner.nextInt(); // Read width
+    System.out.println("Enter your desired number3:");
+    int number3 = scanner.nextInt(); // Read width
+    System.out.println("Enter your desired number4:");
+    int number4 = scanner.nextInt(); // Read width
+    //break
+    int sum = number1+number2+number3+number4;
+    double average = sum/4.0;
+    System.out.println("The sum of the four numbers is " + sum);
+    System.out.println("The average of the four numbers is " + average);
+    
+    
+
+    }
+}
 
 
 //Paste console output below:
